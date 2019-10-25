fa526dac7f151a3314f36ad30f824ec13836b1c8
Igor Beschastnov <beschastnovigor@gmail.com>
2019-10-25 20:10:06 +0300

    Add swagger.

--- backend/conf/settings.py
+++ backend/conf/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    #'rest_framework.authtoken',
+    'drf_yasg',
     'django_extensions',
     'corsheaders',
     'northstar',
